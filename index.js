a4c18598b573422f2cbe60b762fab0aa0dfeabcd
Diogo Lenon <102033927+LenonDiogo@users.noreply.github.com>
2023-02-18 13:13:13 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,55 @@
+const inputPergunta = document.getElementById("inputPergunta");
+const resultadoIA = document.getElementById("resultadoIA");
+
+const OPENAI_API_KEY = "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";
+
+function EnviarPergunta() {
+	var valorPergunta = "criar redação sobre" + inputPergunta.value;
+
+  fetch("https://api.openai.com/v1/completions", {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json", 
+      Authorization: "Bearer " + OPENAI_API_KEY,
+    },
+    body: JSON.stringify({
+      model: "text-davinci-003",
+      prompt: valorPergunta,
+      max_tokens: 600, // tamanho da resposta
+      temperature: 0.7, // criatividade na resposta
+      
+    }),
+  })
+    .then((response) => response.json())
+    .then((json) => {
+      if (resultadoIA.value) resultadoIA.value += "\n";
+
+      if(json.error?.message){
+        resultadoIA.value += `Error: ${json.error.message}`;
+      } else if (json.choices?.[0].text) {
+        var text = json.choices[0].text || "Sem resposta";
+
+        resultadoIA.value += "Chat GPT: " + text;
+      }
+
+      resultadoIA.scrollTop = resultadoIA.scrollHeight;
+    })
+    
+    .catch((error) => console.error("Error:", error))
+    .finally(() => {
+      inputPergunta.value = "";
+      inputPergunta.disabled = false;
+      inputPergunta.focus();
+    });
+
+	if (resultadoIA.value) resultadoIA.value += "\n\n\n";
+
+	resultadoIA.value += `TEMA: ${inputPergunta.value}`;
+	inputPergunta.value = "Carregando...";
+	inputPergunta.disabled = true;
+
+  resultadoIA.scrollTop = resultadoIA.scrollHeight;
+}
+
+document.getElementById("EnviarPergunta").addEventListener("click", EnviarPergunta)
